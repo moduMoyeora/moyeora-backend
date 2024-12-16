8652be4560934bc09491445ecf18ad203f92b0ae
YoungJaeIm <wpem2014@naver.com>
2024-12-16 21:38:49 +0900

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,36 @@
-// import globals from "globals";
-// import pluginJs from "@eslint/js";
-// import tseslint from "typescript-eslint";
-
-
-// /** @type {import('eslint').Linter.Config[]} */
-// export default [
-//   {files: ["**/*.{js,mjs,cjs,ts}"]},
-//   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-//   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-// ];
-
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config} */
-export default {
-  files: ["**/*.{js,mjs,cjs,ts,tsx}"], // 적용 파일 확장자 설정
-  languageOptions: {
-    parser: tsParser, // TypeScript 파서를 사용
-    sourceType: "module", // 모듈 시스템 설정
-    globals: { ...globals.browser, ...globals.node }, // 브라우저 및 Node 글로벌 변수
-  },
-  plugins: {
-    react: reactPlugin,
-    prettier: prettierPlugin,
-  },
-  settings: {
-    react: {
-      version: "detect", // React 버전을 자동 감지
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'], // 적용 파일 확장자 설정
+    languageOptions: {
+      parser: tsParser, // TypeScript 파서를 사용
+      sourceType: 'module', // 모듈 시스템 설정
+      globals: { ...globals.browser, ...globals.node }, // 브라우저 및 Node 글로벌 변수
+    },
+    plugins: {
+      react: reactPlugin,
+      prettier: prettierPlugin,
+      '@typescript-eslint': tseslint,
+    },
+    settings: {
+      react: {
+        version: 'detect', // React 버전을 자동 감지
+      },
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules, // JavaScript 권장 규칙 포함
+      ...tseslint.configs.recommended.rules, // TypeScript 권장 규칙 포함
+      'prettier/prettier': 'error', // Prettier 규칙을 ESLint 에러로 표시
     },
   },
-  extends: [
-    pluginJs.configs.recommended, // 기본 JavaScript 권장 설정
-    tseslint.configs.recommended, // TypeScript 권장 설정
-    prettierConfig, // Prettier와 ESLint 간 충돌 방지
-  ],
-  rules: {
-    "prettier/prettier": "error", // Prettier 규칙을 ESLint 에러로 표시
+  {
+    ignores: ['node_modules/', 'dist/'], // 검사 제외 폴더
   },
-};
+];
