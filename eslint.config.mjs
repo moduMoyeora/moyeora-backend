c1d192ed1415f40585a1d4b75ccdeb8ac1f47d55
subinggrae <subin000602@gmail.com>
2024-12-17 12:44:06 +0900

    chore: 사용하지 않는 라이브러리 삭제

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,12 +22,8 @@ export default [
     rules: {
       ...pluginJs.configs.recommended.rules, // 기본 JavaScript 권장 설정
       ...tseslint.configs.recommended.rules, // TypeScript 권장 설정
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // 사용하지 않는 변수에 대해 경고 (단, 언더스코어로 시작하는 변수는 예외)
       'no-console': 'warn', // `console.log` 사용에 대한 경고
-      '@typescript-eslint/no-explicit-any': 'warn', // `any` 타입 사용에 대한 경고
-      '@typescript-eslint/explicit-module-boundary-types': 'off', // 모듈 경계 타입 강제하지 않음 (Express에서는 유용할 수 있음)
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
-      ...prettierConfig.rules, // Prettier와 ESLint 간 충돌 방지
     },
   },
 ];
