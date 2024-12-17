8e2816d2d362d58530526c4a2781fa8be61cbbc2
subinggrae <subin000602@gmail.com>
2024-12-17 19:50:23 +0900

    feat: CORS 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import userRoute from './routes/userRoute';
 import postRoute from './routes/postRoute';
 import { errorHandler } from './middlewares/errorMiddleware';
+import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -10,6 +11,14 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'https://moyeora.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use('/users', userRoute);
 app.use('/boards/:boardId/posts', postRoute);
